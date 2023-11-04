254c6f71595886dc0700e16b2c011be3df0e3b54
Vagner Zampieri <vagner.zampieri@nuuvem.com>
2023-11-04 16:52:39 -0300

    Implement Welcome

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,12 +1,30 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import { useState } from 'react'
+import {
+  View, Text, TextInput, TouchableOpacity, Image, FlatList
+} from 'react-native'
+import { useRouter } from 'expo-router'
 
 import styles from './welcome.style'
+import { icons, SIZES} from '../../../constants'
 
 const Welcome = () => {
   return (
     <View>
-      <Text>Welcome</Text>
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello Adrian</Text>
+        <Text style={styles.welcomeMessage}>Find your perfect job</Text>
+      </View>
+
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput
+            style={styles.searchInput}
+            value=""
+            onChange={() => {}}
+            placeholder='What are you looking for?'
+          />
+        </View>
+      </View>
     </View>
   )
 }
